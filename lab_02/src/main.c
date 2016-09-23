e0bcdf4b9ec35a68d1562eef226063cf3eebfe61
XRater <kair1998@gmail.com>
2016-09-23 14:38:22 +0300

    test_fix

--- lab_02/src/main.c
+++ lab_02/src/main.c
@@ -5,11 +5,11 @@
 
 int main()
 {
-	char s1[] = "12345";
+	char s1[] = "123456";
 	char s2[] = "1234";
-	printf("strlen: %d\n", test_strlen(s1, 5));
+	printf("strlen: %d\n", test_strlen(s1, 6));
 	printf("strcmp: %d\n", test_strcmp(s1, s2, 1));
-	char s3[] = "6789";
+	char s3[] = "789";
 	printf("strcat: %d\n", test_strcat(s1, s3, "123456789"));
 	char s4[] = "qwerty";
 	printf("strcpy: %d\n", test_strcpy(s1, s4, "qwerty"));
