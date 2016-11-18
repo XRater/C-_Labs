7b28537ee0b4df96172979c33add2abe2fbe311a
Alexander <kair1998@gmail.com>
2016-11-18 03:05:53 +0300

    Ht1

--- ht1/src/bmp.c
+++ ht1/src/bmp.c
@@ -2,68 +2,72 @@
 #include <stdlib.h>
 #include <bmp.h>
 
-void init_bmp(bmp_t* bmp){
+void init_bmp(bmp_t* bmp) {
+//Allocate meaningfull pixels
     bmp->pixels = malloc(bmp->height * sizeof(pixel_t*));
     for (int i = 0; i < bmp->height; i++)
         bmp->pixels[i] = malloc(bmp->width * sizeof(pixel_t));
-
+//Fill alignment pixels with zeores
 	bmp->alignment_size = bmp->width % 4;
 	bmp->alignment = malloc(bmp->alignment_size);   
 	for (int i = 0; i < bmp->alignment_size; i++)
-		bmp->alignment[i] = 255;
+		bmp->alignment[i] = 0;
 }
 
 
-int load_bmp(const char* filename, bmpheader_t* header, bmp_t* bmp){
+int load_bmp(const char* filename, bmpheader_t* header, bmp_t* bmp) {
+//Open file
     FILE* input;
     input = fopen(filename, "rb");
     if (!input) {
         printf("Failed to open input file\n");
         return 1;
     }
-    
+//Read header && init BMP  
     fread(header, sizeof(char), 54, input); 
     bmp->height = header->biHeight;
     bmp->width = header->biWidth;
     bmp->pixel_number = header->biWidth*header->biHeight;
     init_bmp(bmp);
-    for (int i = 1; i <= bmp->height; i++){
+//Read pixels
+    for (int i = 1; i <= bmp->height; i++) {
         fread(bmp->pixels[bmp->height - i], sizeof(pixel_t), bmp->width, input);
     	fread(bmp->alignment, 1, bmp->alignment_size, input);
-    }
-      
+    }      
+//Close file    
     fclose(input);  
     return 0;  
 }
 
 
 int save_bmp(const char* filename, const bmpheader_t* const header, const bmp_t* bmp){
+//Open file
     FILE* output;
     output = fopen(filename, "wb");
+//Write BMP
     fwrite(header, sizeof(char), 54, output);
-    for (int i = 1; i <= bmp->height; i++){
+    for (int i = 1; i <= bmp->height; i++) {
         fwrite(bmp->pixels[bmp->height - i], sizeof(pixel_t), bmp->width, output);
     	fwrite(bmp->alignment, 1, bmp->alignment_size, output);
     }
-    
+//Close file
     fclose(output);  
     return 0;  
 }
 
 
-void free_bmp(bmp_t* bmp){
+void free_bmp(bmp_t* bmp) {
     for (int i = 0; i < bmp->height; i++)
         free(bmp->pixels[i]);
-
     free(bmp->pixels);
  	free(bmp->alignment);
     free(bmp);
 }
 
 
-void mask_bmp(bmp_t* bmp){
+void mask_bmp(bmp_t* bmp) {
     for (int i = 0; i < bmp->height; i++)
-        for (int j = 0; j < bmp->width; j++){
+        for (int j = 0; j < bmp->width; j++) {
             bmp->pixels[i][j].r -= bmp->pixels[i][j].r % 5;
             bmp->pixels[i][j].g -= bmp->pixels[i][j].g % 5;
             bmp->pixels[i][j].b -= bmp->pixels[i][j].b % 5;
@@ -71,23 +75,21 @@ void mask_bmp(bmp_t* bmp){
 }
 
 
-int crop_rotate_bmp(bmpheader_t* header, const bmp_t* bmp, bmp_t* resbmp, size_t x, size_t y, size_t w, size_t h){
+int crop_rotate_bmp(bmpheader_t* header, const bmp_t* bmp, bmp_t* resbmp, size_t x, size_t y, size_t w, size_t h) {
+//Input correctness 
     if (x < 0 || y < 0|| x >= bmp->width || y >= bmp->height){
         printf("Point is out of image\n");
         return 1;
     }
-    
-    if (w <= 0 || h <= 0){
+    if (w <= 0 || h <= 0) {
        printf("Incorrect size\n"); 
        return 1;
     }
-    
-    if (y + h  > bmp->height || x + w  > bmp->width){
+    if (y + h  > bmp->height || x + w  > bmp->width) {
         printf("Second point is out of image\n");
-            return 1;
+        return 1;
     } 
-    
-    
+//Fill new header    
     resbmp->width = h;
     resbmp->height = w;
     init_bmp(resbmp);
@@ -95,10 +97,10 @@ int crop_rotate_bmp(bmpheader_t* header, const bmp_t* bmp, bmp_t* resbmp, size_t
     header->biHeight = w;
     header->bfSize = sizeof(bmpheader_t) + w * (sizeof(pixel_t) * h + resbmp->alignment_size);
     header->biSizeImage = w * (sizeof(pixel_t) * h + resbmp->alignment_size);
+//Fill pixels
   	for (int i = 0; i < w; i++)
   		for (int j = 0; j < h; j++)
   			resbmp->pixels[i][j] = bmp->pixels[y + h - j - 1][x + i];
-
     return 0;
 }
-
+        
--- ht1/src/main.c
+++ ht1/src/main.c
@@ -8,23 +8,28 @@ int main(int argc, char** argv){
         printf("Wrong input format\n");
         return 1;
     }
+//Initialization
     size_t error;
     char* command = argv[1];
     char* input = argv[2];
     char* output = argv[3];
     bmpheader_t* header = malloc(sizeof(bmpheader_t));
     bmp_t* bmp = malloc(sizeof(bmp_t));
+//Open BMP    
     error = load_bmp(input, header, bmp);
     if (error) return 1;
+//Get command
     if (strcmp(command, "crop-rotate") == 0){
         if (argc != 8){
             printf("Wrong input format\n");
             return 1;
         }
+        //Get parametrs
         int x = atoi(argv[4]), y = atoi(argv[5]), w = atoi(argv[6]), h = atoi(argv[7]);  
         bmp_t* resbmp = malloc(sizeof(bmp_t));
         error = crop_rotate_bmp(header, bmp, resbmp, x, y, w, h);
         if (error) return 1;
+        //Save BMP
         error = save_bmp(output, header, resbmp);
         if (error) return 1;
         free_bmp(resbmp);
@@ -33,6 +38,7 @@ int main(int argc, char** argv){
         printf("Unknown command\n");
         return 1;
     }
+//Free memory
     free_bmp(bmp);
     free(header);
     return 0;
