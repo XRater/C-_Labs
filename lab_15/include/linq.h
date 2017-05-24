955ddaf4934093bbb186b01b5e5e9655791ec614
Alexander <kair1998@gmail.com>
2017-05-25 01:46:16 +0300

    testslab_15

--- lab_15/include/linq.h
+++ lab_15/include/linq.h
@@ -22,6 +22,9 @@ class drop_enumerator;
 template<typename T, typename U, typename F>
 class select_enumerator;
 
+template<typename T>
+class take_enumerator;
+
 template<typename T, typename F>
 class until_enumerator;
 
@@ -29,9 +32,9 @@ class until_enumerator;
 template<typename T>
 class enumerator {
 public:
-    virtual T operator*() = 0; // Получает текущий элемент.
+    virtual const T& operator*() const = 0; // Получает текущий элемент.
     virtual enumerator& operator++() = 0;  // Переход к следующему элементу
-    virtual operator bool() = 0;  // Возвращает true, если есть текущий элемент
+    virtual operator bool() const = 0;  // Возвращает true, если есть текущий элемент
 
 
     enumerator<T>& drop(int count) {
@@ -41,12 +44,18 @@ public:
     }
 
     template<typename U = T, typename F>
-    auto select(F func) {}
+    auto select(F func) {
+        return select_enumerator<U, T, F>(*this, func);            
+    }
 
+    auto take(int count) {
+        return take_enumerator<T>(*this, count);
+    }    
 
     template<typename F>
-    auto until(F func) {}
-
+    auto until(F func) {
+        return until_enumerator<T, F>(*this, func);
+    }
 
     std::vector<T> to_vector() {
         std::vector<T> tmp;
@@ -56,7 +65,6 @@ public:
         }
         return tmp;
     }
-
 
     template<typename Iter>
     void copy_to(Iter it) {
@@ -79,11 +87,11 @@ class range_enumerator : public enumerator<T> {
 public:
     range_enumerator(Iter begin, Iter end) : begin_(begin), end_(end) {}
     
-    operator bool() {
+    operator bool() const {
         return begin_ != end_;
     }
     
-    T operator *() {
+    const T& operator *() const {
         if (begin_ == end_)
             throw EnumeratorException("Bad iterator");
         return *begin_;
@@ -123,23 +131,76 @@ private:
 };
 */
 
-/*
+
+template<typename T>
+class take_enumerator : public enumerator<T> {
+public:
+    take_enumerator(enumerator<T> &parent, int count) : parent_(parent), count_(count) {}
+
+    operator bool() const { return count_ > 0; }
+
+    const T& operator *() const { return *parent_; }
+
+    take_enumerator<T>& operator++() { 
+        if (--count_ >= 0) {
+            ++parent_; 
+            return *this;
+        }
+        else
+            throw EnumeratorException("Out of elements");
+    }
+
+private:
+    enumerator<T>& parent_;
+    int count_;
+};
+
+
 template<typename T, typename U, typename F>
 class select_enumerator : public enumerator<T> {
 public:
-  select_enumerator(enumerator<U> &parent, F func) : parent_(parent) {
-  }
+    select_enumerator(enumerator<U> &parent, F func) : parent_(parent), func_(func) {}
+
+    const T& operator*() const {
+        return func_(*parent_);
+    }
+    
+    select_enumerator& operator++() {
+        ++parent_;
+        return *this;    
+    }
+    
+    operator bool() const {
+        return parent_; 
+    }
 
 private:
+    enumerator<U>& parent_;    
+    mutable F func_;
 };
+
 
 template<typename T, typename F>
 class until_enumerator : public enumerator<T> {
 public:
-  until_enumerator(enumerator<T> &parent, F predicate) : parent_(parent) {
-  }
+    until_enumerator(enumerator<T> &parent, F predicate) : parent_(parent), predicate_(predicate) {}
 
+    const T& operator*() const {
+        return *parent_;
+    }
+    
+    until_enumerator& operator++() {
+        ++parent_;
+        return *this;    
+    }
+    
+    operator bool() const {
+        return parent_ && !predicate_(*parent_); 
+    }
+    
 private:
+    enumerator<T>& parent_;
+    mutable F predicate_;
 };
-*/
+
 #endif
